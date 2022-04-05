93633b37e84a268da89583f56e8e1cd493ca1e96
Ilya Savinov <savinovilja1999@gmail.com>
2022-04-05 19:17:30 +0200

    add http service

---
+++ src/services/http.ts
@@ -0,0 +1,70 @@
+import axios from 'axios';
+import { TGenerateOptions, IFormatResponse } from '../types';
+
+const instance = axios.create();
+instance.defaults.baseURL = 'https://www.sicher-im-netz.de/siba-app/siba/';
+instance.defaults.timeout = 180000;
+
+export const httpPost = (url: string, data?: any) =>
+  sendRequest({ method: 'POST', url, data });
+export const httpGet = (url: string, params?: any) =>
+  sendRequest({ method: 'GET', url, params });
+export const httpPatch = (url: string, data?: any) =>
+  sendRequest({ method: 'PATCH', url, data });
+export const httpDelete = (url: string, data?: any) =>
+  sendRequest({ method: 'DELETE', url, data });
+
+const formatResponse: (response?: any) => IFormatResponse<any> = (
+  response = {},
+) => ({
+  headers: response.headers || {},
+  data: response.data || {},
+  status: response.status || 418,
+  statusText: response.statusText || '',
+});
+
+const sendRequest = async ({
+  method,
+  url,
+  data = undefined,
+  params = undefined,
+}: TGenerateOptions) => {
+  const OPTIONS = generateOptions({ method, url, data, params });
+  try {
+    const response = await instance(OPTIONS);
+    return formatResponse(response);
+  } catch (error: any) {
+    if (error.response?.status === 408 || error.code === 'ECONNABORTED') {
+      throw formatResponse({
+        status: 408,
+        statusText: 'Request timeout!!',
+      });
+    }
+    const response = formatResponse(error.response);
+    return response;
+  }
+};
+
+const generateOptions = ({ method, url, data, params }: TGenerateOptions) => {
+  const defaultHeaders = {
+    // Accept: '*/*',
+    'Content-Type': 'application/json',
+
+    // 'Cache-Control': 'no-cache',
+    // 'Accept-Encoding': 'gzip, deflate, br',
+
+    // Accept: 'application/json',
+    // 'Accept-Language': 'ru',
+    'Access-Control-Allow-Origin': '*',
+  };
+
+  return {
+    method,
+    url,
+    data,
+    params,
+    headers: {
+      ...defaultHeaders,
+    },
+  };
+};
