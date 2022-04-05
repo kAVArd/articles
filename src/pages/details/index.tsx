628594de495155a8d1fb7508bf8eb68cfa1537c9
Ilya Savinov <savinovilja1999@gmail.com>
2022-04-05 19:17:40 +0200

    add details page

---
+++ src/pages/details/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Wrapper } from './styles';
+
+const DetailsPage: React.FC = () => {
+  return (
+    <Wrapper>
+      Details
+    </Wrapper>
+  )
+}
+
+export default DetailsPage;
